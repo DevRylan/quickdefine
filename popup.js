5b3ef38e45c4060fc8ec6429b196e3b16e9552b9
Rylan0-0 <everydaynormalvlogs@gmail.com>
2024-07-11 19:09:58 -0500

    Added prototype audio button

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         document.getElementById('word').textContent = '';
         document.getElementById('phonetic').textContent = '';
         document.getElementById('definition').textContent = 'Please enter a word.';
-        return;
+        return; //in case their is no word input
     }
 
     try {
@@ -17,14 +17,26 @@ document.getElementById('searchButton').addEventListener('click', async () => {
 
         const data = await response.json();
         const wordData = data[0];
+        const audioData = wordData.phonetics[0].audio;
         const wordText = wordData.word;
         const phonetic = wordData.phonetic || wordData.phonetics[0].text;
         const definition = wordData.meanings[0].definitions[0].definition;
+        //Formats the text
 
         document.getElementById('word').textContent = `Word: ${wordText}`;
         document.getElementById('phonetic').textContent = `Phonetic: ${phonetic}`;
         document.getElementById('definition').textContent = `Definition: ${definition}`;
-    } catch (error) {
+
+        const newAudioPlayer = document.getElementById('audioPlayer');
+        const newAudioPlayerClone = newAudioPlayer.cloneNode(true);
+        newAudioPlayer.parentNode.replaceChild(newAudioPlayerClone, newAudioPlayer);
+        //Removes the previous event listener
+
+        document.getElementById('audioPlayer').addEventListener('click', ()=>{
+            const audio = new Audio(audioData)
+            audio.play();
+        });
+    } catch (error) { //If there is no definition
         document.getElementById('word').textContent = '';
         document.getElementById('phonetic').textContent = '';
         document.getElementById('definition').textContent = 'Cannot find definition. Maybe check the spelling?';
