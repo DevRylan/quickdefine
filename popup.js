d7f9ff6fff8d8d5bd915c6b969aaaeddf8122717
Rylan0-0 <everydaynormalvlogs@gmail.com>
2024-07-11 19:36:31 -0500

    Added styling to audio button as well as link to searched definition

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         document.getElementById('word').textContent = '';
         document.getElementById('phonetic').textContent = '';
         document.getElementById('definition').textContent = 'Please enter a word.';
+        document.getElementById('audioPlayer').style.display = "none";
         return; //in case their is no word input
     }
 
@@ -18,20 +19,24 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         const data = await response.json();
         const wordData = data[0];
         const audioData = wordData.phonetics[0].audio;
+        const link = wordData.sourceUrls[0];
         const wordText = wordData.word;
         const phonetic = wordData.phonetic || wordData.phonetics[0].text;
         const definition = wordData.meanings[0].definitions[0].definition;
         //Formats the text
 
-        document.getElementById('word').textContent = `Word: ${wordText}`;
-        document.getElementById('phonetic').textContent = `Phonetic: ${phonetic}`;
+        document.getElementById('word').textContent = wordText;
+        document.getElementsByTagName('a')[0].setAttribute('href', link);
+        document.getElementById('phonetic').textContent = phonetic;
         document.getElementById('definition').textContent = `Definition: ${definition}`;
 
+        if(audioData){
+        document.getElementById('audioPlayer').style.display = "inline";
         const newAudioPlayer = document.getElementById('audioPlayer');
         const newAudioPlayerClone = newAudioPlayer.cloneNode(true);
         newAudioPlayer.parentNode.replaceChild(newAudioPlayerClone, newAudioPlayer);
         //Removes the previous event listener
-
+        }
         document.getElementById('audioPlayer').addEventListener('click', ()=>{
             const audio = new Audio(audioData)
             audio.play();
@@ -40,5 +45,6 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         document.getElementById('word').textContent = '';
         document.getElementById('phonetic').textContent = '';
         document.getElementById('definition').textContent = 'Cannot find definition. Maybe check the spelling?';
+        document.getElementById('audioPlayer').style.display = "none";
     }
 });
